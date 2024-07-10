39250085f424272dc330c4cda8ae2750e7e36baa
songjisoo <felixxxxs97@gmail.com>
2024-07-10 23:11:23 +0900

    Add: 폰트어썸 라이브러리 추가

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBarsStaggered, faLocationDot, faLocationPin } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBarsStaggered, faLocationDot)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
