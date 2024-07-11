fa8c2e017b5d326daa82c34f419a9fabb73622a1
songjisoo <felixxxxs97@gmail.com>
2024-07-11 23:30:00 +0900

    Feat: MainComp 추가

---
+++ src/components/MaimComp.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import SearchBar from "./SearchBar.vue";
+    import WeatherInfo from "./WeatherInfo.vue";
+</script>
+
+<template>
+    <div class="container">
+        <SearchBar />
+        <WeatherInfo />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
